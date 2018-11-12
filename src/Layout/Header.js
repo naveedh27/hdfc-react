5251add0a69309d5741e0929e213bde52344d46a
naveedh27 <naveedh27@gmail.com>
2018-11-13 01:54:17 +0530

    master all changes

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -6,7 +6,7 @@ export default class AppHeader extends Component {
 	render() {
 
 		return (
-    <Header as='h1' content='KickStarter Browser' style={ { marginTop: '2.1em', paddingBottom: '2em' } } textAlign='center' />
+			<Header as='h1' content='KickStarter Browser' style={{ marginTop: '2.1em', paddingBottom: '2em' }} textAlign='center' />
 		);
 	}
 
